b27f218acbfecf5ea228ef2024abbe51bcfbb86e
Oluwasegun <oluwasegunmichael8@gmail.com>
2022-11-02 02:56:11 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const path = require("path");
-const fs = require("fs");
+// const fs = require("fs");
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  if (request.url === "/homepage.html") {
-    let filePath = path.join(__dirname, "public", "homepage.html");
+  if (request.url === "/index.html") {
+    let filePath = path.join(__dirname, "public", "index.html");
     fs.readFile(filePath, "utf8", (err, data) => {
       response.writeHead(200, { "Content-Type": "text/html" });
       response.end(data);
